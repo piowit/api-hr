64e946c3e03ca1c748a541da994f3384f2585e63
Piotr Witek <pio.witek@gmail.com>
2025-05-11 11:24:33 +0200

    Add Faraday gem and configure Hibob and Pinpoint initializers with API credentials

---
+++ config/initializers/hibob.rb
@@ -0,0 +1,5 @@
+# https://apidocs.hibob.com/docs/api-service-users
+
+HIBOB_TOKEN = Rails.application.credentials.hibob[:token]
+HIBOB_USER = Rails.application.credentials.hibob[:user]
+HIBOB_API_URL = "https://api.hibob.com/v1"
---
+++ config/initializers/pinpoint.rb
@@ -0,0 +1,4 @@
+# https://developers.pinpointhq.com/docs/introduction
+
+PINPOINT_API_KEY = Rails.application.credentials.pinpoint[:api_key]
+PINPOINT_API_URL = "https://developers-test.pinpointhq.com/api/v1"
