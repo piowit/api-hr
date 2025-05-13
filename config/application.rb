e955689e996416f14523a0dd9994684a3d77a8db
Piotr Witek <pio.witek@gmail.com>
2025-05-13 17:27:49 +0200

    Update gemfile, update database configuration, configure Solid Queue for background job processing; add eager loading for APIs.

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module ApiHr
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.paths.add "app/apis", eager_load: true
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
