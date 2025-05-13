52c21bc01697bca05b7d34d9d8af5dde209c3e46
Piotr Witek <pio.witek@gmail.com>
2025-05-13 19:14:21 +0200

    Remove Hibob and Pinpoint initializers as they are no longer needed.

--- config/initializers/hibob.rb
+++
@@ -1,5 +0,0 @@
-# https://apidocs.hibob.com/docs/api-service-users
-
-HIBOB_TOKEN = Rails.application.credentials.hibob[:token]
-HIBOB_USER = Rails.application.credentials.hibob[:user]
-HIBOB_API_URL = "https://api.hibob.com/v1"
--- config/initializers/pinpoint.rb
+++
@@ -1,4 +0,0 @@
-# https://developers.pinpointhq.com/docs/introduction
-
-PINPOINT_API_KEY = Rails.application.credentials.pinpoint[:api_key]
-PINPOINT_API_URL = "https://developers-test.pinpointhq.com/api/v1"
