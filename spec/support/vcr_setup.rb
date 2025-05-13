990d6aac1ed6b780d78a966d94a849cc8a735979
Piotr Witek <pio.witek@gmail.com>
2025-05-13 19:17:22 +0200

    Add VCR setup configuration for API testing and update rails_helper to load support files

---
+++ spec/support/vcr_setup.rb
@@ -0,0 +1,20 @@
+require 'vcr'
+require 'webmock/rspec'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr_cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.allow_http_connections_when_no_cassette = false
+
+  # Filter sensitive data
+  c.filter_sensitive_data('<PINPOINT_API_KEY>') { Rails.application.credentials.dig(:pinpoint, :api_key) }
+  c.filter_sensitive_data('<HIBOB_USER>') { Rails.application.credentials.dig(:hibob, :user) }
+  c.filter_sensitive_data('<HIBOB_TOKEN>') { Rails.application.credentials.dig(:hibob, :token) }
+
+  c.default_cassette_options = {
+    record: :once,
+    match_requests_on: [:method, :uri, :body],
+    serialize_with: :json
+  }
+end
